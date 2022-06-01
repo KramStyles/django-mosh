ce93b406131020e4d90509a43621864bdf3a3270
kramstyles <mark.eke@decagon.dev>
2022-06-02 00:46:31 +0100

    added urls.py to playground

--- project/urls.py
+++ project/urls.py
@@ -14,7 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
@@ -35,6 +35,7 @@ schema_view = get_schema_view(
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('play/', include('playground.urls')),
 
     # SWAGGER DOCUMENTATION
     path('swagger.json', schema_view.without_ui(cache_timeout=0), name='schema-json'),
