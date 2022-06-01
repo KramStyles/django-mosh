817bc19e1176e39184b4a90919894c41b0c44e30
kramstyles <mark.eke@decagon.dev>
2022-06-02 00:50:00 +0100

    added index.html

--- playground/views.py
+++ playground/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render
 
 
 def hello(request):
-    return HttpResponse('Hello World')
+    return render(request, 'playground/index.html')
