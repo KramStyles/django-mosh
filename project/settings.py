817d7bfed5ed88f1f321b30707f1e038d09fab57
kramstyles <mark.eke@decagon.dev>
2022-06-02 01:20:03 +0100

    added django-debug-toolbar and settings

--- project/settings.py
+++ project/settings.py
@@ -44,9 +44,11 @@ INSTALLED_APPS = [
     # PIP INSTALLS
     'rest_framework',
     'drf_yasg',
+    'debug_toolbar',
 ]
 
 MIDDLEWARE = [
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -128,3 +130,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+INTERNAL_IPS = [
+    '127.0.0.1',
+    'localhost',
+]
