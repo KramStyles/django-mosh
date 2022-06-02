5709aff0eca0941422d2c34d0e04e6d343308804
kramstyles <mark.eke@decagon.dev>
2022-06-02 10:23:03 +0100

    implemented flake8 and created orders model

--- playground/models.py
+++ playground/models.py
@@ -10,8 +10,26 @@ class Product(models.Model):
 
 
 class Customer(models.Model):
+    MEMBERSHIP_CHOICES = [
+        ('B', 'BRONZE'),
+        ('S', 'SILVER'),
+        ('G', 'GOLD')
+    ]
+
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
     email = models.EmailField(max_length=100, unique=True)
     phone = models.CharField(max_length=30)
     birthdate = models.DateTimeField(null=True)
+    membership = models.CharField(choices=MEMBERSHIP_CHOICES, default=MEMBERSHIP_CHOICES[0][0])
+
+
+class Order(models.Model):
+    PAYMENT_CHOICES = [
+        ('P', 'Pending'),
+        ('C', 'Complete'),
+        ('F', 'Failed')
+    ]
+
+    placed_at = models.DateTimeField(auto_now_add=True)
+    payment_status = models.CharField(choices=PAYMENT_CHOICES, default=PAYMENT_CHOICES[0][0])
