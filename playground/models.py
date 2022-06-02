3f71318d51754ac87f376986deea0e6345be6362
kramstyles <mark.eke@decagon.dev>
2022-06-02 09:59:16 +0100

    added models for playground store

--- playground/models.py
+++ playground/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    inventory = models.IntegerField()
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+class Customer(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    email = models.EmailField(max_length=100, unique=True)
+    phone = models.CharField(max_length=30)
+    birthdate = models.DateTimeField(null=True)
