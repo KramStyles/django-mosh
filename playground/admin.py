f4f674ad1cb1de48cd934ccebdfbb4e575326b66
kramstyles <mark.eke@decagon.dev>
2022-06-03 16:48:58 +0100

    introduced @admin.register()

--- playground/admin.py
+++ playground/admin.py
@@ -5,5 +5,11 @@ from . import models
 admin.site.site_header = "Playground Store"
 admin.site.index_title = "E-commerce"
 
+
+@admin.register(models.Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['title', 'price']
+
+
 admin.site.register(models.Collection)
-admin.site.register(models.Product)
+# admin.site.register(models.Product, ProductAdmin)
