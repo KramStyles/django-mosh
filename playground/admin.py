4b464ecfbc4b6d42aeb2dd3a7ae65f9633d025c5
kramstyles <mark.eke@decagon.dev>
2022-06-03 19:43:05 +0100

    added customer admin

--- playground/admin.py
+++ playground/admin.py
@@ -9,6 +9,17 @@ admin.site.index_title = "E-commerce"
 @admin.register(models.Product)
 class ProductAdmin(admin.ModelAdmin):
     list_display = ['title', 'price']
+    list_editable = ['price']
+    list_per_page = 10
+
+
+@admin.register(models.Customer)
+class CustomerAdmin(admin.ModelAdmin):
+    list_per_page = 20
+    list_display = ['first_name', 'last_name', 'membership']
+    list_editable = ['membership']
+    list_filter = ['membership']
+    ordering = ['first_name', 'last_name']
 
 
 admin.site.register(models.Collection)
