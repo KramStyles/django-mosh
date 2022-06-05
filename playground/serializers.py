23b39de4d84143ab9552e1ea9b273ff701708216
kramstyles <mark.eke@decagon.dev>
2022-06-05 14:39:39 +0100

    model serializers

--- playground/serializers.py
+++ playground/serializers.py
@@ -10,11 +10,12 @@ class CollectionSerializer(serializers.Serializer):
     title = serializers.CharField(max_length=25)
 
 
-class ProductSerializer(serializers.Serializer):
-    id = serializers.IntegerField()
-    title = serializers.CharField(max_length=200)
+class ProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.Product
+        fields = ['id', 'title', 'original_price', 'price_with_tax', 'collection']
+
     original_price = serializers.DecimalField(max_digits=6, decimal_places=2, source='price')
-    description = serializers.CharField(max_length=20000)
     price_with_tax = serializers.SerializerMethodField(method_name='calculate_tax')
     # collection = serializers.StringRelatedField()  # Requires loading product and collections together
     collection = CollectionSerializer()
