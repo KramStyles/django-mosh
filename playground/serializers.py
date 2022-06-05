2740950da0b6f28d567bc6ed60e2262316e140b6
kramstyles <mark.eke@decagon.dev>
2022-06-05 14:28:11 +0100

    serializing relationship as nested object

--- playground/serializers.py
+++ playground/serializers.py
@@ -5,13 +5,19 @@ from rest_framework import serializers
 from . import models
 
 
+class CollectionSerializer(serializers.Serializer):
+    id = serializers.IntegerField()
+    title = serializers.CharField(max_length=25)
+
+
 class ProductSerializer(serializers.Serializer):
     id = serializers.IntegerField()
     title = serializers.CharField(max_length=200)
     original_price = serializers.DecimalField(max_digits=6, decimal_places=2, source='price')
     description = serializers.CharField(max_length=20000)
     price_with_tax = serializers.SerializerMethodField(method_name='calculate_tax')
-    collection = serializers.StringRelatedField()  # Requires loading product and collections together
+    # collection = serializers.StringRelatedField()  # Requires loading product and collections together
+    collection = CollectionSerializer()
 
     def calculate_tax(self, product: models.Product):
         answer = product.price * Decimal(1.14)
