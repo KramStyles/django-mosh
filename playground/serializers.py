6dca828431f8d6e73a49794d3f0ada5bf408493b
kramstyles <mark.eke@decagon.dev>
2022-06-05 14:04:25 +0100

    added custom field with serialize method field

--- playground/serializers.py
+++ playground/serializers.py
@@ -1,6 +1,8 @@
+from decimal import Decimal
+
 from rest_framework import serializers
 
-# from . import models
+from . import models
 
 
 class ProductSerializer(serializers.Serializer):
@@ -8,3 +10,7 @@ class ProductSerializer(serializers.Serializer):
     title = serializers.CharField(max_length=200)
     price = serializers.DecimalField(max_digits=6, decimal_places=2)
     description = serializers.CharField(max_length=20000)
+    price_with_tax = serializers.SerializerMethodField(method_name='calculate_tax')
+
+    def calculate_tax(self, product: models.Product):
+        return product.price * Decimal(1.2)
