ecf6f7bdfd4a006af061a829e0fb457edd8d9063
kramstyles <mark.eke@decagon.dev>
2022-07-09 00:16:43 +0100

    created user profile

--- playground/models.py
+++ playground/models.py
@@ -1,5 +1,7 @@
 from uuid import uuid4
 
+from django.conf import settings
+from django.contrib import admin
 from django.core import validators
 from django.db import models
 
@@ -49,15 +51,26 @@ class Customer(models.Model):
         ('G', 'GOLD')
     ]
 
-    first_name = models.CharField(max_length=100)
-    last_name = models.CharField(max_length=100)
-    email = models.EmailField(max_length=100, unique=True)
     phone = models.CharField(max_length=30)
     birthdate = models.DateTimeField(null=True)
     membership = models.CharField(max_length=20, choices=MEMBERSHIP_CHOICES, default=MEMBERSHIP_CHOICES[0][0])
 
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+
     def __str__(self):
-        return self.email
+        return f"{self.user.email} | {self.user.first_name} {self.user.last_name}"
+
+    # Added this for sorting purposes in admin panel
+    @admin.display(ordering='user__first_name')
+    def first_name(self):
+        return self.user.first_name
+
+    @admin.display(ordering='user__last_name')
+    def last_name(self):
+        return self.user.last_name
+
+    class Meta:
+        ordering = ['user__first_name', 'user__last_name']
 
 
 class Order(models.Model):
