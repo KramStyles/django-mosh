348b08f6c51d52209df7c5e1678a3c36599f060a
kramstyles <mark.eke@decagon.dev>
2022-07-09 21:15:59 +0100

    changed djoser get user serializer

--- project/settings.py
+++ project/settings.py
@@ -150,6 +150,7 @@ SIMPLE_JWT = {
 DJOSER = {
     'SERIALIZERS': {
         'user_create': 'core.serializers.UserCreateSerializer',
+        'current_user': 'core.serializers.UserSerializer',
     }
 }
 
